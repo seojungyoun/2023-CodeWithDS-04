128dff83aaa0908058222fab425436449143ad96
bangyewon <byewon1111@gmail.com>
2023-08-24 03:09:46 +0900

    grad_real

--- grad_fin 4/gradpage/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from gradpage import views
-
-urlpatterns = [
-    path('',views.main, name="main"),
-    path('createArchiving/', views.create_archiving.as_view()),
-
-]
